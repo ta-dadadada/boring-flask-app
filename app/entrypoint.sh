cffe2dc8f9c336b9e99023fe6f8acd309096f70a
Yoshikatsu Tada <yoshikatsu.tada@e-seikatsu.co.jp>
2019-04-09 13:05:58 +0900

    enable Ctrl-C key to kill uwsgi forgroud process

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 MODULE_PATH=src/app.py
-uwsgi \
+exec uwsgi \
+  --master \
   --plugins http \
   --plugins python3 \
   --plugins logfile \
   --http :8080\
   --wsgi-file ${MODULE_PATH} \
-  --callable app
+  --callable app \
